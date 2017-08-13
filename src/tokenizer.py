1aaf2a93d823415ded06b4d882dc4c07deb525b2
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-13 11:31:13 +0700

    Add Tokenizer class

---
+++ src/tokenizer.py
@@ -0,0 +1,29 @@
+from re import sub
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+
+class Tokenizer:
+    def __init__(self, text):
+        self.text = text
+
+    def word_tokenize(self):
+        # lowercase text
+        self.text = self.text.lower()
+
+        # filter punctuation
+        sub(r'\W+', '', self.text)
+
+        # tokenize text into words
+        word_tokens = word_tokenize(self.text)
+
+        # filter stop words
+        stop_words = set(stopwords.words('english'))
+        word_tokens = [word for word in word_tokens if word not in stop_words]
+
+        # stem words
+        word_tokens = [PorterStemmer().stem(word) for word in word_tokens]
+
+        return word_tokens
