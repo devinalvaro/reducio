212e8590fc4d14c3966dc311ae5a43ddfa235889
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-14 10:26:48 +0700

    Tokenizer no longer a class

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -4,23 +4,19 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
-class Tokenizer:
-    def __init__(self, text):
-        self.text = text
+def tokenize_word(text):
+    # lowercase text
+    text = text.lower()
 
-    def word_tokenize(self):
-        # lowercase text
-        self.text = self.text.lower()
+    # filter punctuation
+    pattern = re.compile('[^A-Za-z -]+')
+    text = pattern.sub('', text)
 
-        # filter punctuation
-        pattern = re.compile('[^A-Za-z -]+')
-        self.text = pattern.sub('', self.text)
+    # tokenize text into words
+    word_tokens = word_tokenize(text)
 
-        # tokenize text into words
-        word_tokens = word_tokenize(self.text)
+    # filter stop words
+    stop_words = set(stopwords.words('english'))
+    word_tokens = [word for word in word_tokens if word not in stop_words]
 
-        # filter stop words
-        stop_words = set(stopwords.words('english'))
-        word_tokens = [word for word in word_tokens if word not in stop_words]
-
-        return word_tokens
+    return word_tokens
