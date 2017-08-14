51ce07f653a2350a89dbfda26d770a7e4578b0f4
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-14 16:16:11 +0700

    Improve pre-processing input article

--- src/reducto.py
+++ src/reducto.py
@@ -1,10 +1,12 @@
-from corpusreader import CorpusReader
 from articlesummarizer import ArticleSummarizer
+from corpusreader import CorpusReader
 
 corpus_type = input("What kind of article do you want to summarize? ")
 corpus_reader = CorpusReader(corpus_type)
 
-article = input("Please put the article here:\n")
+with open('data/article.txt', 'r') as file:
+    article = file.read().replace('\n', ' ').replace('\r', '')
+
 summary = ArticleSummarizer(article,
                             len(corpus_reader.articles),
                             corpus_reader.document_frequency)
