eff6f11aabbaf074f3420738787f51414d992945
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-15 16:25:24 +0700

    Use Pandas and JSON

--- src/corpus_reader.py
+++ src/corpus_reader.py
@@ -1,4 +1,5 @@
-from csv import DictReader
+from json import dump, load
+from pandas import read_csv
 
 from tokenizer import tokenize_word
 
@@ -19,25 +20,29 @@ class CorpusReader:
         Count the document frequency of each word.
         """
 
-        self.filepath = 'data/news.csv'
+        try:
+            with open('data/news.json', 'r') as jsonfile:
+                self.document_frequency = load(jsonfile)
+        except IOError:
+            with open('data/news.json', 'w') as jsonfile:
+                self.open_corpus()
+                self.count_document_frequency()
 
-        self.open_corpus()
-        self.count_document_frequency()
+                dump(self.document_frequency, jsonfile)
 
     def open_corpus(self):
         """Open corpus dataset and store it in self.articles"""
 
-        with open(self.filepath, newline='', encoding='iso-8859-1') as csvfile:
-            reader = DictReader(csvfile)
-            for entry in reader:
-                self.articles.append(entry['ctext'])
+        with open('data/news.csv') as csvfile:
+            reader = read_csv(csvfile)
+            for entry in reader['ctext']:
+                self.articles.append(str(entry))
 
     def count_document_frequency(self):
         """Count the document frequency of each word"""
 
         for article in self.articles:
             words = set(tokenize_word(article, only_noun=False))
-
             for word in words:
                 if word not in self.document_frequency:
                     self.document_frequency[word] = 1
