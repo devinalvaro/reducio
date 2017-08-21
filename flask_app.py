8fb6f264a5ba88c9400bfae98e01578e5ae30ef4
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-21 16:09:58 +0700

    Handle pasting article to article.txt

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    with open('data/article.txt', 'w') as file:
+        file.write(request.form.get('text'))
+
 
 if __name__ == '__main__':
     app.run()
--- static/main.js
+++ static/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    // Summarize Function //
     function summarize(text) {
-        $("#boxes").html(text)
+        $.ajax({
+            url: "/test",
+            type: "POST",
+            data: {
+                'text': text
+            }
+        });
     };
 
-    // Click Events //
     $("#sum-btn").click(function() {
         var text = $("#text-box").val();
         summarize(text);
