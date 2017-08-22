314ee798895a1d95b6ab6f4830e793253013ca12
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-22 19:06:28 +0700

    Replace double quotes to single quotes in AJAX's JSON

--- static/main.js
+++ static/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
     function summarize(text, number) {
         $.ajax({
-            url: "/reducio",
-            type: "POST",
+            url: '/reducio',
+            type: 'POST',
             data: {
                 'text': text,
                 'number': number
             },
             success: function(data) {
-                $("#summarized-text-box").css('background-color', '#66bb6a');
-                $("#summarized-text-box").html(data);
+                $('#summarized-text-box').css('background-color', '#66bb6a');
+                $('#summarized-text-box').html(data);
             }
         });
     };
 
-    $("#sum-btn").click(function() {
-        var text = $("#text-box").val();
-        var number = $("#number-box").val();
+    $('#sum-btn').click(function() {
+        var text = $('#text-box').val();
+        var number = $('#number-box').val();
         summarize(text, number);
     });
 });
